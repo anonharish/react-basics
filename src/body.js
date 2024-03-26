f56df3989182b3590c0f6a3f6d741ec4ad0a3764
Harish K <harish.s@ictsystemsindia.com>
2024-03-26 10:09:37 +0530

    NO TICKET:commited my changes

--- src/body.js
+++ src/body.js
@@ -2,8 +2,18 @@ import React from 'react'
 import RestuarantCard from './components/restuarantCard';
 
 const Body = () => {
+const getTopRatedRestaurant =()=>{
+  console.log("hello brooo");
+}
+
+React.useEffect(()=>{
+ let  restuarantsData=fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4398772&lng=78.36573419999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING").then(data=>data.json());
+ console.log(restuarantsData,"insideUseEfferct");
+},[])
+
   return (
     <div className='body-container'>
+      <button className='btn-primary' onClick={getTopRatedRestaurant}>Top Rated Restauarant</button>
       <RestuarantCard/>
     </div>
   )
